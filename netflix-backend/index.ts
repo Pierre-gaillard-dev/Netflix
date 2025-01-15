7930239d217f2b2931dcd9c55d9d797faaaf1fb8
Pierre <pierre.gaillard56@orange.fr>
2025-01-15 13:54:16 +0100

    finished configuration of the back-end environment

--- netflix-backend/index.ts
+++ netflix-backend/index.ts
@@ -1,12 +1,25 @@
-import express from 'express';
-const app = express();
+import express from "express"
+import dotenv from "dotenv"
+import sequelize from "./config/sequelize"
+import routes from "./routes"
 
-app.use(express.json());
+dotenv.config()
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000!');
-})
+/* Middlewares */
+app.use(express.json())
+app.use("/api", routes)
+
+sequelize
+	.authenticate()
+	.then(() => {
+		console.log("Database connected successfully!")
+		app.listen(PORT, () => {
+			console.log(`Server is running on http://localhost:${PORT}`)
+		})
+	})
+	.catch((error) => {
+		console.error("Database connection failed:", error)
+	})
