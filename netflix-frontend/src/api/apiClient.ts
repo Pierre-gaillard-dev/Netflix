c826656aebc3b796b071b80c8784371765d00280
Pierre <pierre.gaillard.dev@gmail.com>
2025-03-12 15:52:18 +0100

    [frontend] quick switch between dev and prod api using comments

--- netflix-frontend/src/api/apiClient.ts
+++ netflix-frontend/src/api/apiClient.ts
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const apiClient = axios.create({
 	baseURL: "https://netflix.pierre-gaillard.mds-vannes.yt/api",
+	//baseURL: "http://192.168.1.19:3000/api",
 	withCredentials: true,
 	headers: {
 		"Content-Type": "application/json",
